f300606ad56689906542402f0d9625dd5645f139
Ali Shahbazi <alishahbazi799@gmail.com>
2023-09-05 13:56:16 -0400

    Current Weather has been completed

--- OpenWeatherMap/Services/Service.cs
+++ OpenWeatherMap/Services/Service.cs
@@ -9,149 +9,253 @@ using OpenWeatherMap.Models;
 
 namespace OpenWeatherMap.Services
 {
-     public class Service
- {
-     private readonly Config _config;
-     private readonly HttpClient _httpClient;
-     private bool _disposed;
+    public class Service
+    {
+        private readonly Config _config;
+        private readonly HttpClient _httpClient;
+        private bool _disposed;
 
-     public Service(Config config, Action<HttpClientHandler> httpClientHandler = null)
-     {
-         _config = config ?? throw new ArgumentNullException(nameof(config), "Config cannot be null.");
+        public Service(Config config, Action<HttpClientHandler> httpClientHandler = null)
+        {
+            _config = config ?? throw new ArgumentNullException(nameof(config), "Config cannot be null.");
 
-         var handler = new HttpClientHandler();
-         httpClientHandler?.Invoke(handler);
+            var handler = new HttpClientHandler();
+            httpClientHandler?.Invoke(handler);
 
-         _httpClient = new HttpClient(handler)
-         {
-             BaseAddress = new Uri(_config.BaseUrl)
-         };
-     }
+            _httpClient = new HttpClient(handler)
+            {
+                BaseAddress = new Uri(_config.BaseUrl)
+            };
+        }
 
-     public async Task<CurrentWeatherModel> GetForecastByCityNameAsync(string city, Action<HttpClientHandler> httpClientHandler = null)
-     {
-         if (string.IsNullOrWhiteSpace(city))
-             throw new ArgumentNullException(nameof(city), "City name cannot be null or empty.");
+        public async Task<CurrentWeatherModel> GetCurrentWeatherByCityNameAsync(
+            string city,
+            Action<HttpClientHandler> httpClientHandler = null)
+        {
+            if (string.IsNullOrWhiteSpace(city))
+                throw new ArgumentNullException(nameof(city), "City name cannot be null or empty.");
 
-         try
-         {
-             var sBuilder = new StringBuilder();
-             sBuilder.Append("weather?q=");
-             sBuilder.Append(city);
-             sBuilder.Append("&appid=");
-             sBuilder.Append(_config.ApiKey);
+            try
+            {
+                var sBuilder = new StringBuilder();
+                sBuilder.Append("weather?q=");
+                sBuilder.Append(city);
+                sBuilder.Append("&appid=");
+                sBuilder.Append(_config.ApiKey);
 
-             var httpResponse = await _httpClient.GetAsync(sBuilder.ToString());
-             httpResponse.EnsureSuccessStatusCode();
+                var httpResponse = await _httpClient.GetAsync(sBuilder.ToString());
+                httpResponse.EnsureSuccessStatusCode();
 
-             if (!httpResponse.IsSuccessStatusCode)
-             {
-                 if (httpResponse.StatusCode == HttpStatusCode.NotFound)
-                     throw new Exception("City not found.");
-                 throw new Exception($"API returned a {httpResponse.StatusCode} status.");
-             }
-             switch (httpResponse.StatusCode)
-             {
-                 case HttpStatusCode.NotFound:
-                     throw new Exception($"{city} not found.");
-                 case HttpStatusCode.Unauthorized:
-                     throw new Exception("Invalid API key or unauthorized access.");
-                 case HttpStatusCode.BadRequest:
-                     throw new Exception("Bad request. Please check the request parameters.");
-                 default:
-                 {
-                     if (!httpResponse.IsSuccessStatusCode)
-                     {
-                         throw new Exception($"API returned a {httpResponse.StatusCode} status.");
-                     }
+                if (!httpResponse.IsSuccessStatusCode)
+                {
+                    if (httpResponse.StatusCode == HttpStatusCode.NotFound)
+                        throw new Exception("City not found.");
+                    throw new Exception($"API returned a {httpResponse.StatusCode} status.");
+                }
 
-                     break;
-                 }
-             }
+                switch (httpResponse.StatusCode)
+                {
+                    case HttpStatusCode.NotFound:
+                        throw new Exception($"{city} not found.");
+                    case HttpStatusCode.Unauthorized:
+                        throw new Exception("Invalid API key or unauthorized access.");
+                    case HttpStatusCode.BadRequest:
+                        throw new Exception("Bad request. Please check the request parameters.");
+                    default:
+                    {
+                        if (!httpResponse.IsSuccessStatusCode)
+                        {
+                            throw new Exception($"API returned a {httpResponse.StatusCode} status.");
+                        }
 
-             var responseContent = await httpResponse.Content.ReadAsStringAsync();
-             // Print the JSON response string to the console
-              //Console.WriteLine(responseContent);
-             return JsonSerializer.Deserialize<CurrentWeatherModel>(responseContent);
-         }
-         catch (HttpRequestException ex)
-         {
-             // Catch other HttpRequestException instances not handled by the above catch blocks
-             throw new Exception("There was a problem with the HTTP request.", ex);
-         }
-         catch (JsonException ex)
-         {
-             throw new Exception("Invalid JSON response received.", ex);
-         }
-         catch (ArgumentNullException ex)
-         {
-             throw new Exception("The city name cannot be null or empty.", ex);
-         }
-         catch (Exception ex)
-         {
-             // General catch block to catch any other exceptions
-             throw new Exception("An unexpected error occurred.", ex);
-         }
-     }
-     
-     public async Task<WeatherForecastModel> GetForecastByZipCodeAsync(string zipCode, string countryCode = "", Action<HttpClientHandler> httpClientHandler = null)
-     {
-         if (string.IsNullOrWhiteSpace(zipCode))
-         {
-             throw new ArgumentNullException(nameof(zipCode), "ZIP code cannot be null or empty.");
-         }
+                        break;
+                    }
+                }
 
-         var query = !string.IsNullOrEmpty(countryCode) ? $"{zipCode},{countryCode}" : zipCode;
+                var responseContent = await httpResponse.Content.ReadAsStringAsync();
+                // Print the JSON response string to the console
+                //Console.WriteLine(responseContent);
+                return JsonSerializer.Deserialize<CurrentWeatherModel>(responseContent);
+            }
+            catch (HttpRequestException ex)
+            {
+                // Catch other HttpRequestException instances not handled by the above catch blocks
+                throw new Exception("There was a problem with the HTTP request.", ex);
+            }
+            catch (JsonException ex)
+            {
+                throw new Exception("Invalid JSON response received.", ex);
+            }
+            catch (ArgumentNullException ex)
+            {
+                throw new Exception("The city name cannot be null or empty.", ex);
+            }
+            catch (Exception ex)
+            {
+                // General catch block to catch any other exceptions
+                throw new Exception("An unexpected error occurred.", ex);
+            }
+        }
 
-         try
-         {
-             
-             var sBuilder = new StringBuilder();
-             sBuilder.Append("forecast?zip=");
-             sBuilder.Append(query);
-             sBuilder.Append("&appid=");
-             sBuilder.Append(_config.ApiKey);
-             
-             var httpResponse = await _httpClient.GetAsync(sBuilder.ToString());
-             httpResponse.EnsureSuccessStatusCode();
+        public async Task<CurrentWeatherModel> GetCurrentWeatherByGeoCoordinatesAsync(
+            double latitude,
+            double longitude,
+            Action<HttpClientHandler> httpClientHandler = null)
+        {
+            try
+            {
+                var sBuilder = new StringBuilder();
+                sBuilder.Append("weather?lat=");
+                sBuilder.Append(latitude);
+                sBuilder.Append("&lon=");
+                sBuilder.Append(longitude);
+                sBuilder.Append("&appid=");
+                sBuilder.Append(_config.ApiKey);
 
-             switch (httpResponse.StatusCode)
-             {
-                 case HttpStatusCode.NotFound:
-                     throw new Exception($"{zipCode} not found.");
-                 case HttpStatusCode.Unauthorized:
-                     throw new Exception("Invalid API key or unauthorized access.");
-                 case HttpStatusCode.BadRequest:
-                     throw new Exception("Bad request. Please check the request parameters.");
-                 default:
-                 {
-                     if (!httpResponse.IsSuccessStatusCode)
-                     {
-                         throw new Exception($"API returned a {httpResponse.StatusCode} status.");
-                     }
+                var httpResponse = await _httpClient.GetAsync(sBuilder.ToString());
+                httpResponse.EnsureSuccessStatusCode();
 
-                     break;
-                 }
-             }
+                switch (httpResponse.StatusCode)
+                {
+                    case HttpStatusCode.NotFound:
+                        throw new Exception($"{latitude},{longitude} not found.");
+                    case HttpStatusCode.Unauthorized:
+                        throw new Exception("Invalid API key or unauthorized access.");
+                    case HttpStatusCode.BadRequest:
+                        throw new Exception("Bad request. Please check the request parameters.");
+                    default:
+                    {
+                        if (!httpResponse.IsSuccessStatusCode)
+                        {
+                            throw new Exception($"API returned a {httpResponse.StatusCode} status.");
+                        }
 
-            
-             var responseContent = await httpResponse.Content.ReadAsStringAsync();
-             var response = JsonSerializer.Deserialize<WeatherForecastModel>(responseContent);
-             
-             return response;
-         }
-         catch (HttpRequestException ex)
-         {
-             throw new Exception("There was a problem with the HTTP request.", ex);
-         }
-     }
-     public void Dispose()
-     {
-         if (_disposed)
-             return;
-         
-         _httpClient.Dispose();
-         _disposed = true;
-     }
- }
+                        break;
+                    }
+                }
+
+                var responseContent = await httpResponse.Content.ReadAsStringAsync();
+                var response = JsonSerializer.Deserialize<CurrentWeatherModel>(responseContent);
+
+                return response;
+            }
+            catch (HttpRequestException ex)
+            {
+                throw new Exception("There was a problem with the HTTP request.", ex);
+            }
+        }
+
+        public async Task<CurrentWeatherModel> GetCurrentWeatherByCityIdAsync(
+            int cityId,
+            Action<HttpClientHandler> httpClientHandler = null)
+        {
+            try
+            {
+                var sBuilder = new StringBuilder();
+                sBuilder.Append("weather?id=");
+                sBuilder.Append(cityId);
+                sBuilder.Append("&appid=");
+                sBuilder.Append(_config.ApiKey);
+
+                var httpResponse = await _httpClient.GetAsync(sBuilder.ToString());
+                httpResponse.EnsureSuccessStatusCode();
+
+                switch (httpResponse.StatusCode)
+                {
+                    case HttpStatusCode.NotFound:
+                        throw new Exception($"{cityId} not found.");
+                    case HttpStatusCode.Unauthorized:
+                        throw new Exception("Invalid API key or unauthorized access.");
+                    case HttpStatusCode.BadRequest:
+                        throw new Exception("Bad request. Please check the request parameters.");
+                    default:
+                    {
+                        if (!httpResponse.IsSuccessStatusCode)
+                        {
+                            throw new Exception($"API returned a {httpResponse.StatusCode} status.");
+                        }
+
+                        break;
+                    }
+                }
+
+                var responseContent = await httpResponse.Content.ReadAsStringAsync();
+                var response = JsonSerializer.Deserialize<CurrentWeatherModel>(responseContent);
+
+                return response;
+            }
+            catch (HttpRequestException ex)
+            {
+                throw new Exception("There was a problem with the HTTP request.", ex);
+            }
+        }
+
+        public async Task<WeatherForecastModel> GetForecastByZipCodeAsync(
+            string zipCode,
+            string stateCode = "",
+            string countryCode = "",
+            Action<HttpClientHandler> httpClientHandler = null)
+        {
+            if (string.IsNullOrWhiteSpace(zipCode))
+            {
+                throw new ArgumentNullException(nameof(zipCode), "ZIP code cannot be null or empty.");
+            }
+
+            // Check if the state code and country code is given
+            var query = zipCode;
+            if (!string.IsNullOrEmpty(stateCode))
+                query = $"{query},{stateCode}";
+            if (!string.IsNullOrEmpty(countryCode))
+                query = $"{query},{countryCode}";
+
+            try
+            {
+                var sBuilder = new StringBuilder();
+                sBuilder.Append("forecast?zip=");
+                sBuilder.Append(query);
+                sBuilder.Append("&appid=");
+                sBuilder.Append(_config.ApiKey);
+
+                var httpResponse = await _httpClient.GetAsync(sBuilder.ToString());
+                httpResponse.EnsureSuccessStatusCode();
+
+                switch (httpResponse.StatusCode)
+                {
+                    case HttpStatusCode.NotFound:
+                        throw new Exception($"{zipCode} not found.");
+                    case HttpStatusCode.Unauthorized:
+                        throw new Exception("Invalid API key or unauthorized access.");
+                    case HttpStatusCode.BadRequest:
+                        throw new Exception("Bad request. Please check the request parameters.");
+                    default:
+                    {
+                        if (!httpResponse.IsSuccessStatusCode)
+                        {
+                            throw new Exception($"API returned a {httpResponse.StatusCode} status.");
+                        }
+
+                        break;
+                    }
+                }
+
+                var responseContent = await httpResponse.Content.ReadAsStringAsync();
+                var response = JsonSerializer.Deserialize<WeatherForecastModel>(responseContent);
+
+                return response;
+            }
+            catch (HttpRequestException ex)
+            {
+                throw new Exception("There was a problem with the HTTP request.", ex);
+            }
+        }
+
+        public void Dispose()
+        {
+            if (_disposed)
+                return;
+
+            _httpClient.Dispose();
+            _disposed = true;
+        }
+    }
 }
--- TestingService/Program.cs
+++ TestingService/Program.cs
@@ -13,8 +13,10 @@ namespace TestingService
             var service = new Service(new Config());
             try
             {
-                 var result = await service.GetForecastByCityNameAsync("London");
+                // var result = await service.GetCurrentWeatherByCityNameAsync("London");
+                // var result = await service.GetCurrentWeatherByGeoCoordinatesAsync(44.34, 10.99);
                 // var result = await service.GetForecastByZipCodeAsync("94040");
+                var result = await service.GetCurrentWeatherByCityIdAsync(2172797);
                 Console.WriteLine(JsonSerializer.Serialize(result));
             }
             catch (Exception ex)
