83d32742665e88aab74ba23462309d797ce92cf4
Ali Shahbazi <alishahbazi799@gmail.com>
2023-09-05 13:23:32 -0400

    Upgrade to .NetCore3.1

---
+++ OpenWeatherMap/Configuration/Config.cs
@@ -0,0 +1,8 @@
+﻿namespace OpenWeatherMap.Configuration
+{
+    public class Config
+    {
+        public string ApiKey { get; set; } = "e5d16ddc7303ea59c5f741319c2fd446";
+        public string BaseUrl { get; set; } = "https://api.openweathermap.org/data/2.5/";
+    }
+}
---
+++ OpenWeatherMap/Services/IntFromStringJsonConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace OpenWeatherMap.Services
+{
+    public class IntFromStringJsonConverter : JsonConverter<int>
+    {
+        public override int Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            switch (reader.TokenType)
+            {
+                case JsonTokenType.String:
+                {
+                    var stringValue = reader.GetString();
+                    if (int.TryParse(stringValue, out int intValue))
+                    {
+                        return intValue;
+                    }
+
+                    break;
+                }
+                case JsonTokenType.Number:
+                    return reader.GetInt32();
+            }
+
+            throw new JsonException();
+        }
+
+        public override void Write(Utf8JsonWriter writer, int value, JsonSerializerOptions options)
+        {
+            writer.WriteNumberValue(value);
+        }
+    }
+}
