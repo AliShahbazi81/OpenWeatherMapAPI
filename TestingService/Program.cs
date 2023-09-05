2d1c07709f4daf92708d07cf87d0846c3b07f292
Ali Shahbazi <alishahbazi799@gmail.com>
2023-09-05 13:25:52 -0400

    Add Testing Console

---
+++ TestingService/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using OpenWeatherMap.Configuration;
+using OpenWeatherMap.Services;
+
+namespace TestingService
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var service = new Service(new Config());
+            try
+            {
+                 var result = await service.GetForecastByCityNameAsync("London");
+                // var result = await service.GetForecastByZipCodeAsync("94040");
+                Console.WriteLine(JsonSerializer.Serialize(result));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
